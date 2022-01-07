20444fdba47a235f092fa68d064e89b16fa955e1
Rusty_Brakes <george_cochrane@hotmail.com>
2022-01-07 14:36:59 +0000

    Colemak is gone, compiled and tested well

--- keyboards/gmmk/pro/iso/keymaps/rustybrakes/config.h
+++ keyboards/gmmk/pro/iso/keymaps/rustybrakes/config.h
@@ -29,8 +29,3 @@
     #define RGB_DISABLE_WHEN_USB_SUSPENDED
 #endif
 
-// add fifth layer for colemak  -- set "COLEMAK_LAYER_ENABLE = yes" in rules.mk to enable
-#if defined COLEMAK_LAYER_ENABLE
-    #define DYNAMIC_KEYMAP_LAYER_COUNT 5
-    #define _COLEMAK 4
-#endif // COLEMAK_LAYER_ENABLE
